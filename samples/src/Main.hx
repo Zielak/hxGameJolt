4a122c4e14c72e10c4375e3eb43e3d47339a1c82
Darek Greenly <git@zielak.pl>
2015-08-23 10:40:06 +0200

    first one

---
+++ samples/src/Main.hx
@@ -0,0 +1,34 @@
+
+import luxe.Input;
+
+import hxgamejolt.GameJolt;
+
+
+class Main extends luxe.Game {
+
+    override function config(config:luxe.AppConfig) {
+
+        return config;
+
+    } //config
+
+    override function ready() {
+
+        GameJolt.
+
+    } //ready
+
+    override function onkeyup( e:KeyEvent ) {
+
+        if(e.keycode == Key.escape) {
+            Luxe.shutdown();
+        }
+
+    } //onkeyup
+
+    override function update(dt:Float) {
+
+    } //update
+
+
+} //Main
